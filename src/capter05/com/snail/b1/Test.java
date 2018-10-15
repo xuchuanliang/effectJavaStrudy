49226a02cb561e0871ad652022bb221fd8531f2b
snail <xuchuanliang361@163.com>
2018-10-15 08:11:03 +0800

    提交

---
+++ src/capter05/com/snail/b1/Test.java
@@ -0,0 +1,15 @@
+package capter05.com.snail.b1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        test(list);
+    }
+
+    public static void test(List<?> what){
+
+    }
+}
