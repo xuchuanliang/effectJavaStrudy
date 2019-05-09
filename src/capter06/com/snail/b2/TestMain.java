0be191d04abf1bc75589963e7343e993391afccd
xuchuanliang <xuchuanliang361@163.com>
2019-05-09 20:27:23 +0800

    提交

--- src/capter06/com/snail/b2/TestMain.java
+++ src/capter06/com/snail/b2/TestMain.java
@@ -1,12 +1,7 @@
 package capter06.com.snail.b2;
 
-/**
- * @author xuchuanliangbt
- * @title: TestMain
- * @projectName effectJavaStrudy
- * @description:
- * @date 2019/5/98:13
- * @Version
- */
 public class TestMain {
+    public static void main(String[] args){
+        System.out.println("".equals("ss"));
+    }
 }
